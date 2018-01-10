ce86a2bf4f4543287ffe41af4f856b6780427c30
zhouwei <zhouwei11@58ganji.com>
2018-01-10 21:09:15 +0800

    test

--- app/src/main/java/com/zhouwei/helloapt/activity/TestActivity.java
+++ app/src/main/java/com/zhouwei/helloapt/activity/TestActivity.java
@@ -34,12 +34,11 @@ public class TestActivity extends Activity {
 
         View view = null;
         Activity activity = null;
-<<<<<<< HEAD
+
+        
         ///////////////////////////计算机/////////////////////////////
 
         ////////////////////////////*************/////////////////////////
-=======
->>>>>>> parent of 159024c... test03
     }
 
     public void bindViewOnClick(View v) {
