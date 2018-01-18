0b1915cc2a1b94d1e4f95c474c42e51d8a6bf622
zhouwei <zhouwei11@58ganji.com>
2018-01-18 17:20:12 +0800

    review

--- app/src/main/java/com/zhouwei/helloapt/activity/TestCountDownTimerViewActivity.java
+++ app/src/main/java/com/zhouwei/helloapt/activity/TestCountDownTimerViewActivity.java
@@ -1,15 +1,27 @@
 package com.zhouwei.helloapt.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.zhouwei.helloapt.R;
+import com.zhouwei.helloapt.view.CountDownTimerView;
 
 public class TestCountDownTimerViewActivity extends AppCompatActivity {
+
+    private com.zhouwei.helloapt.view.CountDownTimerView ctv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_count_down_timer_view);
+        this.ctv = (CountDownTimerView) findViewById(R.id.ctv);
+
+        ctv.addClickListener(new CountDownTimerView.ClickListener() {
+            @Override
+            public void onClick(View v) {
+                ctv.start();
+            }
+        });
     }
 }
